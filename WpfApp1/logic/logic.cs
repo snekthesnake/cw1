dcec1db7b3831610393aa159027b145683674c46
snekthesnake <autistgera3@gmail.com>
2024-03-16 20:03:09 +0300

    added pifagor's formula

---
+++ WpfApp1/logic/logic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.logic
+{
+    public class logic
+    {
+        static double Calculate(int a, int b, int angle)
+        {
+            double c = Math.Pow(a, 2) + Math.Pow(b, 2) - (2 * b * a) * Math.Cos(angle);
+
+            return (Math.Sqrt(c));
+        }
+    }
+}
